5feae37a0cc15823e4636e83af341651937fb8dd
Lexi Ngo Mbea <lexi.ngombea3@yahoo.com>
2024-10-09 15:02:51 +0200

    Création Entities

---
+++ back/src/main/java/com/assistavie/back/entities/Chatbot.java
@@ -0,0 +1,13 @@
+package com.assistavie.back.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Chatbot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
